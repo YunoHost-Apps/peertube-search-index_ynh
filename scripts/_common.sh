397bb7402eb62b588b415db02a50f3c8105e3dd1
Félix Piédallu <felix@piedallu.me>
2024-02-28 13:12:21 +0100

    fix install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,8 +13,8 @@ NODEJS_VERSION=16
 _git_clone_or_pull() {
     repo_dir="$1"
 
-    upstream_repository=$(ynh_read_manifest --manifest_key='.upstream.code')
-    upstream_url=$(ynh_read_manifest --manifest_key='.resources.sources.main.url')
+    upstream_repository=$(ynh_read_manifest --manifest_key='upstream.code')
+    upstream_url=$(ynh_read_manifest --manifest_key='resources.sources.main.url')
     upstream_commit=$(echo "$upstream_url" | sed -e "s|^${upstream_repository}/-/archive/\([^/]*\)/.*|\1|")
 
     if [ -d "$repo_dir" ]; then
