f7080ee0c4c3de4cda4edc02a7822296dd86cbc5
Félix Piédallu <felix@piedallu.me>
2024-02-28 12:03:57 +0100

    initial manifestv2

--- doc/PRE_INSTALL.md
+++ doc/PRE_INSTALL.md
@@ -1,7 +1,3 @@
 ## Important points to read before installing
 
 1. **PeerTube search-index** requires a dedicated **root domain**, e.g. search.domain.tld
-
-## Configuration
-
-To configure this app: modify the file `/var/www/peertube-search-index/config/production.yaml` with SSH.
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,20 +4,27 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="git"
-
-extra_pkg_dependencies="elasticsearch-oss"
-
 NODEJS_VERSION=16
-
-REPOSITORY="https://framagit.org/framasoft/peertube/search-index"
-
-COMMIT="316adda0db184cb36f0645a60dc8f6a1611ecd28"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+_git_clone_or_pull() {
+    repo_dir="$1"
+
+    upstream_repository=$(ynh_read_manifest --key='.upstream.code')
+    upstream_url=$(ynh_read_manifest --key='.resources.sources.main.url')
+    upstream_commit=$(echo "$upstream_url" | sed -e "s|^${upstream_repository}/-/archive/\([^/]*\)/.*|\1|")
+
+    if [ -d "$repo_dir" ]; then
+        ynh_exec_as "$app" git -C "$repo_dir" fetch --quiet
+    else
+        ynh_exec_as "$app" git clone "$upstream_repository" "$repo_dir" --quiet
+    fi
+    ynh_exec_as "$app" git -C "$repo_dir" checkout "$upstream_commit"
+    ynh_exec_as "$app" git -C "$repo_dir" submodule update --init --recursive
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
